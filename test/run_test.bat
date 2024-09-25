915bc272210dc5c266daf3f409d502e677cb3d0c
Fabricio Ribeiro <fabricio.ribeiro@espressif.com>
2024-09-25 11:47:00 +0100

    Finish execution instructions. Update script files.

--- test/run_test.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-:: Enter test folder
-cd AutoTest
-
-:: install node modules
-npm ci
-
-:: run tests
-npm run AllTest
--- test/run_test.sh
+++ test/run_test.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# Save the arguments as environment variables
+export EIM_FILE_PATH="$1"
+export EIM_VERSION="$2"
+
 #Enter test folder
-cd AutoTest
+cd script
 
 # install node modules
 npm ci
 
 # run tests
-npm run AllTest
+npm run test
