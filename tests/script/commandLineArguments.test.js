ffda81f5d1c8bd5e0fe4588f8ccd194c1d5ea46d
Fabricio Ribeiro <fabricio.ribeiro@espressif.com>
2024-10-02 15:55:05 +0100

    Updated test class, simplified basic test launch

--- tests/script/commandLineArguments.test.js
+++ tests/script/commandLineArguments.test.js
@@ -1,6 +1,6 @@
 import { expect } from "chai";
 import { describe, it, before, after, beforeEach, afterEach } from "mocha";
-import { InteractiveCLITestRunner } from "./CLITestRunner.js"
+import { InteractiveCLITestRunner } from "./CLITestRunner.class.js";
 import os from "os";
 import path from "path";
 
@@ -18,7 +18,7 @@ if (process.env.EIM_VERSION) {
 } else {
     eimVersion = "idf-im-cli 0.1.0";
 }
-export function runLineArgumentsTests(){
+export function runLineArgumentsTests() {
     describe("CLI Line Arguments Tests", function () {
         let testRunner;
 
@@ -26,31 +26,34 @@ export function runLineArgumentsTests(){
             testRunner = new InteractiveCLITestRunner(pathToEim);
         });
 
-        after(async function () { //Need to check if this is necessary
+        after(async function () {
+            //Need to check if this is necessary
             if (testRunner) {
-            await testRunner.stop();
+                await testRunner.stop();
             }
         });
 
         describe("Command-line Arguments", function () {
             it("should handle valid arguments", async function () {
-            const { output, code } = await testRunner.runWithArgs(["-V"]);
-            expect(code).to.equal(0);
-            expect(output).to.include(eimVersion);
+                const { output, code } = await testRunner.runWithArgs(["-V"]);
+                expect(code).to.equal(0);
+                expect(output).to.include(eimVersion);
             });
 
             it("should show help with --help argument", async function () {
-            const { output, code } = await testRunner.runWithArgs(["--help"]);
-            expect(code).to.equal(0);
-            expect(output).to.include("Usage:");
-            expect(output).to.include("Options:");
+                const { output, code } = await testRunner.runWithArgs([
+                    "--help",
+                ]);
+                expect(code).to.equal(0);
+                expect(output).to.include("Usage:");
+                expect(output).to.include("Options:");
             });
 
             it("should handle invalid arguments", async function () {
-            const { output, code } = await testRunner.runWithArgs(["-KK"]);
-            expect(code).to.not.equal(0);
-            expect(output).to.include("unexpected argument");
+                const { output, code } = await testRunner.runWithArgs(["-KK"]);
+                expect(code).to.not.equal(0);
+                expect(output).to.include("unexpected argument");
             });
         });
     });
-};
+}
