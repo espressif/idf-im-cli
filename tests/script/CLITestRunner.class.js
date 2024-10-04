0373db8c349098a0ddf1977d4ccc950f6185a607
Fabricio Ribeiro <fabricio.ribeiro@espressif.com>
2024-10-04 14:23:48 +0100

    Re-enable CI Build job, remove debug msgs

--- tests/script/CLITestRunner.class.js
+++ tests/script/CLITestRunner.class.js
@@ -46,7 +46,7 @@ export class InteractiveCLITestRunner {
             this.process.onData((data) => {
                 try {
                     this.output += data;
-                    console.log(data);
+                    // console.log(data);
                 } catch (error) {
                     console.error("Error in onData:", error);
                     this.error = error;
