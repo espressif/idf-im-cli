4f865df86e91b2ef4e41d036eaa34ad3cee42679
Fabricio-ESP <fabricio.ribeiro@espressif.com>
2024-11-20 17:16:16 +0000

    Add non interactive test

---
+++ tests/runs/nonInteractiveInstall.test.js
@@ -0,0 +1,62 @@
+import { describe, it, before, after } from "mocha";
+import { runPostInstallTest } from "../script/postInstall.test.js";
+import { runInstallNonInteractive } from "../script/installNonInteractive.test.js";
+import logger from "../classes/logger.class.js";
+import os from "os";
+import path from "path";
+
+/**
+ * Setup the following environmental variables to execute this test:
+ *
+ * EIM_FILE_PATH to point to the eim application.
+ *
+ * use:
+ * Windows: $env:<variable>="<value>"
+ * Linux/mac: export <variable>="<value>"
+ *
+ */
+
+let pathToEim;
+
+if (process.env.EIM_FILE_PATH) {
+    pathToEim = process.env.EIM_FILE_PATH;
+} else {
+    pathToEim = path.join(os.homedir(), "eim-cli/eim");
+}
+
+logger.debug(`Starting non-interactive installation using EIM on ${pathToEim}`);
+
+const targetList = ["esp32c6"]; // targets used for IDF installation
+const idfVersionList = ["v5.3.1"]; // IDF versions to be installed
+const installFolder = ".espressif3";
+const projectFolder = "project";
+
+let installArgs = [];
+installArgs.push(`-p ${path.join(os.homedir(), installFolder)}`); // Install Path
+installArgs.push(`-t ${targetList.join(",")}`); // Targets (in case of multiple separate with ,)
+installArgs.push(`-i ${idfVersionList.join(",")}`); // IDF versions (in case of multiple separate with ,)
+installArgs.push(`-m https://github.com`); // IDF tools mirror
+installArgs.push(`--idf-mirror https://github.com`); // ESP-IDF mirror
+installArgs.push(`-r true`); // recursive submodules init
+
+const pathToIDFScript =
+    os.platform() !== "win32"
+        ? path.join(installPath, `activate_idf_${idfVersionList[0]}.sh`)
+        : path.join(
+              installPath,
+              idfVersionList[0],
+              `Microsoft.PowerShell_profile.ps1`
+          );
+
+describe("Installation using non-interactive settings", function () {
+    this.timeout(2400000);
+
+    runInstallNonInteractive(pathToEim, installArgs);
+
+    runPostInstallTest(
+        pathToIDFScript,
+        path.join(os.homedir(), installFolder, projectFolder),
+        targetList[0],
+        "esp32c6"
+    );
+});
---
+++ tests/script/installNonInteractive.test.js
@@ -0,0 +1,76 @@
+import { expect } from "chai";
+import { describe, it, before, after, beforeEach, afterEach } from "mocha";
+import { InteractiveCLITestRunner } from "../classes/CLITestRunner.class.js";
+import logger from "../classes/logger.class.js";
+
+export function runInstallNonInteractive(pathToEim, args = []) {
+    describe("run custom installation using non interactive terminal", function () {
+        let testRunner = null;
+
+        before(async function () {
+            logger.debug(`Starting non interactive installation`);
+            logger.debug(`Using parameters ${args.join("")}`);
+            this.timeout(5000);
+            testRunner = new InteractiveCLITestRunner();
+            await testRunner.start();
+        });
+
+        afterEach(function () {
+            if (this.currentTest.state === "failed") {
+                logger.info(
+                    `Terminal output on failure: >>\r ${testRunner.output}`
+                );
+                testRunner.sendInput("\x03");
+            }
+        });
+
+        after(async function () {
+            logger.info("Custom installation routine completed");
+            this.timeout(10000);
+            try {
+                await testRunner.stop(6000);
+            } catch {
+                logger.debug("Error to clean up terminal after test");
+            }
+        });
+
+        /** Run installation with non-interactive shell.
+         *
+         * It is expected for all setting to fall back to default in case it is not passed as argument
+         * Config file would also overwrite default setting.
+         *
+         */
+
+        it("Should install IDF using specified parameters", async function () {
+            logger.info("Sent command line for IDF installation");
+            testRunner.sendInput(`${pathToEim} ${args.join("")} -n true \r`);
+
+            const installationSuccessful = await testRunner.waitForOutput(
+                "Successfully installed IDF",
+                1200000
+            );
+
+            expect(
+                installationSuccessful,
+                "Failed to complete installation, missing 'Successfully Installed IDF'"
+            ).to.be.true;
+
+            expect(
+                testRunner.output,
+                "Error message during installation"
+            ).to.not.include("error");
+
+            expect(
+                testRunner.output,
+                "Failed to complete installation, missing 'Now you can start using IDF tools'"
+            ).to.include("Now you can start using IDF tools");
+
+            if ("-r true" in args || "--recursive-submodules" in args) {
+                expect(
+                    testRunner.output,
+                    "Failed to download submodules, missing 'Finished fetching submodules'"
+                ).to.include("Finished fetching submodules");
+            }
+        });
+    });
+}
